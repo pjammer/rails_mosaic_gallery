d8792065f9f4c1f139e5693334bcacf0f0435b04
pjammer <philipjingram@yahoo.com>
2011-08-31 16:29:37 -0400

    should add the right number of ends.

--- lib/rails_mosaic_gallery.rb
+++ lib/rails_mosaic_gallery.rb
@@ -5,24 +5,13 @@ module ActionView
       %Q{
       <script type="text/javascript">
       $(function($) {
-        	$('.#{bar2}').mosaic({
+        	$('.#{arg}').mosaic({
 		animation : 'slide' //fade or slide
 		});
       });
       </script>
       }.html_safe
     end
-    <<-EOL
-    		<div class="mosaic-block bar2">
-			<a href="http://www.nonsensesociety.com/2010/12/i-am-not-human-portraits/" target="_blank" class="mosaic-overlay">
-				<div class="details">
-					<h4>I Am Not Human - Unique Self Portraits</h4><br/>
-					<p>via the Nonsense Society (photo credit: Dan Deroches)</p>
-				</div>
-			</a>
-			<div class="mosaic-backdrop"><img src="http://buildinternet.s3.amazonaws.com/projects/mosaic/desroches.jpg"/></div>
-		</div>
-    EOL
     def mosaic_tag(path_to_image, mosaic_option, link_address, content = {})
       #pass an object and a mosaic option in.
       #content hash to have :heading and :paragraph in it
@@ -35,6 +24,7 @@ module ActionView
         end
         div_for(:class => "mosaic-backdrop") do
           image_tag(path_to_image)
+        end
       end
     end
   end
