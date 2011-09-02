d88c6d823d86cdf41b90ab0a093f7b63b4703216
pjammer <philipjingram@yahoo.com>
2011-09-01 20:54:30 -0400

    added mosaic tag fixing and comments for examples

--- lib/rails_mosaic_gallery.rb
+++ lib/rails_mosaic_gallery.rb
@@ -1,34 +1,38 @@
 module ActionView
   module Helpers
     #First you require the scripts, then you can use the mosaic tag.
+    #Example using an image uploaded via the paperclip gem:
+    <<-EOL
+    <%= mosaic_scripts %>
+    <% @shops.each do |shop| %>
+      <%= mosaic_tag(shop.logo.url(:medium),
+  		    "bar2",
+  		    shop_path(shop.name),
+  		    {:heading => shop.name, :description =>(truncate(shop.description, :length => 30)) })  %>
+    <% end %>
+    EOL
     def mosaic_scripts(arg = 'bar2')
-          %Q{
-          <link href="/stylesheets/mosaic_plugin.css" media="screen" rel="stylesheet" type="text/css" />
-          <script type="text/javascript" src="/javascripts/mosaic_plugin/mosaic.1.0.1.min.js"></script>
-          <script type="text/javascript">
-          $(function($) {
-            	$('.#{arg}').mosaic({
-    		animation : 'slide' //fade or slide
-    		});
-          });
-          </script>
-          }.html_safe
-    end
-        
-    # You need an image url, pick a mosaic plugin 'theme', a link to link to and content for those that need it.
-    def mosaic_tag(path_to_image, mosaic_option, link_address, content = {})
-      #pass an object and a mosaic option in.
       %Q{
-        <div class="mosaic-block #{mosaic_option}">
-                 <a href="#{link_address}" class="mosaic-overlay">
-                        <div class="details">
-                             <h4>#{content[:heading]}</h4><br/>
-          <p>#{content[:description]}</p>
-                         </div>
-                  </a>
-          <div class="mosaic-backdrop"><img src="#{path_to_image}"/></div>
-        </div>
-        }.html_safe
+<link href="/stylesheets/mosaic_plugin.css" media="screen" rel="stylesheet" type="text/css" />
+      <script type="text/javascript" src="/javascripts/mosaic_plugin/mosaic.1.0.1.min.js"></script>
+      <script type="text/javascript">
+      $(function($) {
+        	$('.#{arg}').mosaic({
+		animation : 'slide' //fade or slide
+		});
+      });
+      </script>
+      }.html_safe
+    end
+      #content hash to have :heading and :paragraph in it
+    def mosaic_tag(path_to_image, mosaic_option, link_address, content = {})
+      content_tag(:div, :class => "mosaic-block #{mosaic_option}") {
+        content_tag(:a, {:href => link_address, :class => "mosaic-overlay"}) {
+          content_tag(:div, :class => "details") {
+            content_tag(:h4, content[:heading]) + content_tag(:p, content[:description])
+          }
+        } + content_tag(:div, image_tag(path_to_image.to_s), {:class => "mosaic-backdrop"})
+      }
     end
   end
 end
